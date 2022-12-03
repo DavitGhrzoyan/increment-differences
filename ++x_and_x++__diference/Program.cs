660b2635fd453bd113c43acf549eb2fd685e3057
Davit <ghrzoyandavit@gmail.com>
2022-12-03 21:31:43 +0400

    Add project files.

---
+++ ++x_and_x++__diference/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace __x_and_x____diference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = 5;
+            int sum = value++ + ++value + ++value - --value - value--;
+            //          5     +    7    +    8    -    7     -  7
+            //value? = 6
+            //sum ? =  6
+            //sum = 5; without post pre
+
+            // if it is pre then the sign is before  ->   force action;
+            // if it is post then the sign is after  ->   snooze action
+
+            Console.WriteLine("The value is " + value + " and sum is " + sum);
+        }
+    }
+}
